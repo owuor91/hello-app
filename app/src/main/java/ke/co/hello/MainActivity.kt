04a3bafa013a07dddd0a04bf79e24a0963927dd0
John Owuor <owuor91@gmail.com>
2020-07-24 15:02:58 +0300

    added registration layout and registration activity intent

--- app/src/main/java/ke/co/hello/MainActivity.kt
+++ app/src/main/java/ke/co/hello/MainActivity.kt
@@ -1,5 +1,6 @@
 package ke.co.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,5 +9,10 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    tvRegister.setOnClickListener {
+      val intent = Intent(baseContext, RegistrationActivity::class.java)
+      startActivity(intent)
+    }
   }
 }
---
+++ app/src/main/java/ke/co/hello/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package ke.co.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
