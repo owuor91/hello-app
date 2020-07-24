6719145d73d291b750f4baef5b4bb283050b464e
John Owuor <owuor91@gmail.com>
2020-07-24 16:52:21 +0300

    capture user input from the fields, add icons to layouts

--- app/src/main/java/ke/co/hello/MainActivity.kt
+++ app/src/main/java/ke/co/hello/MainActivity.kt
@@ -3,6 +3,7 @@ package ke.co.hello
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     tvRegister.setOnClickListener {
       val intent = Intent(baseContext, RegistrationActivity::class.java)
       startActivity(intent)
+    }
+
+    btnLogin.setOnClickListener {
+      var userName = etUserName.text.toString()
+      var password = etPassword.text.toString()
+      Toast.makeText(baseContext, password, Toast.LENGTH_SHORT).show()
     }
   }
 }
--- app/src/main/java/ke/co/hello/RegistrationActivity.kt
+++ app/src/main/java/ke/co/hello/RegistrationActivity.kt
@@ -2,10 +2,22 @@ package ke.co.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        btnRegister.setOnClickListener {
+            var firstName = etFirstName.text.toString()
+            var lastName = etLastName.text.toString()
+            val email = etEmail.text.toString()
+            val phoneNumber = etPhoneNumber.text.toString()
+            val password = etPassword.text.toString()
+            val passwordConfirmation = etConfirmPassword.text.toString()
+            Toast.makeText(baseContext, lastName, Toast.LENGTH_SHORT).show()
+        }
     }
 }
