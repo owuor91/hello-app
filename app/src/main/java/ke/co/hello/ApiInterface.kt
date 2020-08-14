9712b93da8ad51e34d15b4f7735361279821db3b
John Owuor <owuor91@gmail.com>
2020-08-14 17:43:45 +0300

    implement user registration

---
+++ app/src/main/java/ke/co/hello/ApiInterface.kt
@@ -0,0 +1,12 @@
+package ke.co.hello
+
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+interface ApiInterface {
+    @POST("register")
+    fun registerStudent(@Body requestBody: RequestBody): Call<RegistrationResponse>
+}
--- app/src/main/java/ke/co/hello/RegistrationActivity.kt
+++ app/src/main/java/ke/co/hello/RegistrationActivity.kt
@@ -1,9 +1,15 @@
 package ke.co.hello
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_registration.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +23,42 @@ class RegistrationActivity : AppCompatActivity() {
             val phoneNumber = etPhoneNumber.text.toString()
             val password = etPassword.text.toString()
             val passwordConfirmation = etConfirmPassword.text.toString()
+
+            var requestBody = MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("first_name", firstName)
+                .addFormDataPart("last_name", lastName)
+                .addFormDataPart("email", email)
+                .addFormDataPart("phone_number", phoneNumber)
+                .addFormDataPart("password", password)
+                .build()
+
+            registerUser(requestBody)
             Toast.makeText(baseContext, lastName, Toast.LENGTH_SHORT).show()
         }
+    }
+
+
+    fun registerUser(requestBody: RequestBody) {
+        var apiClient = ApiClient.buildService(ApiInterface::class.java)
+        var registrationCall = apiClient.registerStudent(requestBody)
+        registrationCall.enqueue(object : Callback<RegistrationResponse> {
+            override fun onFailure(call: Call<RegistrationResponse>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<RegistrationResponse>,
+                response: Response<RegistrationResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Toast.makeText(baseContext, response.body()?.message, Toast.LENGTH_LONG).show()
+                    startActivity(Intent(baseContext, MainActivity::class.java))
+                } else {
+                    Toast.makeText(baseContext, response.errorBody().toString(), Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        })
     }
 }
