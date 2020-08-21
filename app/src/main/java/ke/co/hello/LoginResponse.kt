a9b02651863170a273795c5ac14345079baea690
John Owuor <owuor91@gmail.com>
2020-08-21 16:04:49 +0300

    implement login api call and save access token to shared preferences

---
+++ app/src/main/java/ke/co/hello/LoginResponse.kt
@@ -0,0 +1,8 @@
+package ke.co.hello
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    @SerializedName("access_token") var accessToken: String,
+    @SerializedName("message") var message: String
+)
--- app/src/main/java/ke/co/hello/MainActivity.kt
+++ app/src/main/java/ke/co/hello/MainActivity.kt
@@ -1,10 +1,17 @@
 package ke.co.hello
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +24,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     btnLogin.setOnClickListener {
-      var userName = etUserName.text.toString()
+      var email = etUserName.text.toString()
       var password = etPassword.text.toString()
-      Toast.makeText(baseContext, password, Toast.LENGTH_SHORT).show()
-      startActivity(Intent(baseContext, CoursesActivity::class.java))
+
+      val requestBody = MultipartBody.Builder()
+        .setType(MultipartBody.FORM)
+        .addFormDataPart("email", email)
+        .addFormDataPart("password", password)
+        .build()
+
+      loginUser(requestBody)
     }
+  }
+
+  fun loginUser(requestBody: RequestBody){
+    val apiClient = ApiClient.buildService(ApiInterface::class.java)
+    val loginCall = apiClient.loginStudent(requestBody)
+
+    loginCall.enqueue(object :Callback<LoginResponse> {
+      override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+        Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
+      }
+
+      override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+        if (response.isSuccessful){
+          Toast.makeText(baseContext, response.body()?.message, Toast.LENGTH_LONG).show()
+          var accessToken = response.body()?.accessToken
+          var sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
+          var editor = sharedPreferences.edit()
+          editor.putString("ACCESS_TOKEN_KEY", accessToken)
+          editor.apply()
+          val intent = Intent(baseContext, CoursesActivity::class.java)
+          startActivity(intent)
+          finish()
+        }
+        else{
+          Toast.makeText(baseContext, response.errorBody().toString(), Toast.LENGTH_LONG).show()
+        }
+      }
+    })
   }
 }
